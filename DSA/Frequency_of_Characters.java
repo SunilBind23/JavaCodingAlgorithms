b02824bad1ab97af5fb8d75c8ce01b4513713c87
Sunil Bind <153701952+SunilBind23@users.noreply.github.com>
2026-06-16 17:41:04 +0530

    Add character frequency counting in Frequency_of_Characters
    Implemented a program to count the frequency of characters in a given string.

--- DSA/Frequency_of_Characters.java
+++ DSA/Frequency_of_Characters.java
@@ -1,5 +1,31 @@
 package DSA;
 
 public class Frequency_of_Characters {
+/*
+  Input:  "hello"
 
+Output:
+h -> 1
+e -> 1
+l -> 2
+o -> 1
+  */
+  public static void main(String[] args) {
+
+        String str = "hello";
+
+        for (int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+            int count = 0;
+
+            for (int j = 0; j < str.length(); j++) {
+                if (ch == str.charAt(j)) {
+                    count++;
+                }
+            }
+
+            System.out.println(ch + " -> " + count);
+        }
+    }
 }

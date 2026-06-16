c3afa52b76da84d44ca4b7d998130b906e78dace
Sunil Bind <153701952+SunilBind23@users.noreply.github.com>
2026-06-16 17:46:44 +0530

    Add functionality to print even-length words from string
    Implemented a Java program to print words with even lengths from a given string.

--- DSA/Print_Even_Word_From_a_String.java
+++ DSA/Print_Even_Word_From_a_String.java
@@ -1,5 +1,32 @@
 package DSA;
 
 public class Print_Even_Word_From_a_String {
+  /* Input:
+"I am learning Java"
+
+Words:
+I        -> length 1 (odd)
+am       -> length 2 (even)
+learning -> length 8 (even)
+Java     -> length 4 (even)
+
+Output:
+am
+learning
+Java
+  */
+   public static void main(String[] args) {
+
+        String str = "I am learning Java";
+
+        String[] words = str.split(" ");
+
+        for (String word : words) {
+
+            if (word.length() % 2 == 0) {
+                System.out.println(word);
+            }
+        }
+    }
 
 }

c3dcda64fa7427037196276a4d90b880a9e250cb
Sunil Bind <153701952+SunilBind23@users.noreply.github.com>
2026-06-16 17:45:06 +0530

    Add logic to move all zeros to the end of the array
    Implemented a method to move all zeros in an array to the end while maintaining the order of non-zero elements.

--- DSA/Move_All_Zeros_to_the_End.java
+++ DSA/Move_All_Zeros_to_the_End.java
@@ -1,5 +1,32 @@
 package DSA;
 
 public class Move_All_Zeros_to_the_End {
+/* Input:  [1, 0, 2, 0, 3, 4]
 
+Output: [1, 2, 3, 4, 0, 0]
+  */
+   public static void main(String[] args) {
+
+        int[] arr = {1, 0, 2, 0, 3, 4};
+
+        int index = 0;
+
+        // Non-zero elements ko front me lao
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                arr[index] = arr[i];
+                index++;
+            }
+        }
+
+        // Remaining positions me 0 bhar do
+        while (index < arr.length) {
+            arr[index] = 0;
+            index++;
+        }
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
 }

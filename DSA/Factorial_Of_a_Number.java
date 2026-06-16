9fd41b888e62571e02e39f6afd37a9aa5728ec8d
Sunil Bind <153701952+SunilBind23@users.noreply.github.com>
2026-06-16 17:36:55 +0530

    updated

--- DSA/Factorial_Of_a_Number.java
+++ DSA/Factorial_Of_a_Number.java
@@ -1,5 +1,20 @@
 package DSA;
 
 public class Factorial_Of_a_Number {
+  //5! = 5 × 4 × 3 × 2 × 1 = 120
+  //4! = 4 × 3 × 2 × 1 = 24
+  //3! = 3 × 2 × 1 = 6
+  //0! = 1
+  public static void main(String[] args) {
+        int num = 5;
+        int fact = 1;
+
+        for (int i = 1; i <= num; i++) {
+            fact = fact * i;
+        }
+
+        System.out.println("Factorial = " + fact);
+    }
+
 
 }

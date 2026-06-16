884054f42f9dd1ba8b006a4e7bf75d5bf14ae382
Sunil Bind <153701952+SunilBind23@users.noreply.github.com>
2026-06-16 17:42:33 +0530

    Implement Length of Last Word in String

--- DSA/Length_of_Last_Word_in_String.java
+++ DSA/Length_of_Last_Word_in_String.java
@@ -1,5 +1,17 @@
 package DSA;
 
 public class Length_of_Last_Word_in_String {
+/* Input:  "Hello World"
+Output: 5
+  */
+   public static void main(String[] args) {
 
+        String str = "Hello World";
+
+        str = str.trim();
+
+        String[] words = str.split(" ");
+
+        System.out.println(words[words.length - 1].length());
+    }
 }
